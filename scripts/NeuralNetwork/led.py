8d0f75b5da2f227682172d08727d4d5768a573b4
Catta76 <fede99catta@gmail.com>
2023-09-18 15:56:34 +0200

    testing led

--- scripts/NeuralNetwork/led.py
+++ scripts/NeuralNetwork/led.py
@@ -20,7 +20,7 @@ class LED:
         pass
     
     #TODO: prendere dense e conv da file
-    def build_autoencoder(self,latent_dim, input_shape, dense, conv, activation='relu',dropout_rate=0.2):
+    def build_autoencoder(self,latent_dim, input_shape, dense, conv, activation='relu',dropout_rate=0.2, verbose=True):
         
         #README: pool not wanted -> set 0
         
@@ -40,11 +40,14 @@ class LED:
         E.append(tfkl.InputLayer(input_shape=(input_shape)))   
         
         for layer in conv:
-            E.append(tfkl.conv2D(filters=layer[0],kernel_size=(layer[1],layer[1])),padding='same',activation=activation,kernel_initializer=tfk.initializers.GlorotUniform(self.seed))
+            E.append(tfkl.Conv2D(filters=layer[0],kernel_size=(layer[1],layer[1]),padding='same',activation=activation,kernel_initializer=tfk.initializers.GlorotUniform(self.seed)))
             E.append(tfkl.MaxPool2D(pool_size=(2,2)))
-            x =  (x + x%2) /2
+            #x =  (x + x%2) /2
+            x = x//2
 
-        E.append(tfkl.flatten())
+        print(x)
+        
+        E.append(tfkl.Flatten())
 
         for layer in dense:
             E.append(tfkl.Dense(units=layer,activation=activation))
@@ -57,14 +60,15 @@ class LED:
         # (None,latent_dim)
         # (None, dense_dim)
 
-        for layer in dense:
-            D.insert(0,tfkl.Dense(units=layer,activation=activation))
-            D.insert(0,tfkl.Dropout(dropout_rate,seed=self.seed))
+        for layer in np.flip(dense):
+            D.append(tfkl.Dense(units=layer,activation=activation))
+            D.append(tfkl.Dropout(dropout_rate,seed=self.seed))
 
-
-        D.append(tfkl.Reshape(x,x,dense[0]/(x**2)))
-        for layer in np.reverse(conv):
-            D.append(tfkl.Conv2DTranspose(layer[0], kernel_size=(layer[1],layer[1]), stride=(2,2), padding='same', activation=activation))
+        x = np.int32(x)
+        D.append(tfkl.Reshape((x,x,dense[0]//(x**2))))
+        
+        for layer in np.flip(conv):
+            D.append(tfkl.Conv2DTranspose(layer[0], kernel_size=(layer[1],layer[1]), strides=(2,2), padding='same', activation=activation))
 
         self.encoder = tfk.Sequential(E)
         self.decoder = tfk.Sequential(D)
@@ -72,10 +76,11 @@ class LED:
         ae_input = tfkl.Input(shape=(input_shape))
         encoded_input = self.encoder(ae_input)
         ae_output = self.decoder(encoded_input)
-        self.autoencoder = tfk.Model(inputs=(ae_input), outputs=ae_output, names='autoencoder')
+        self.autoencoder = tfk.Model(inputs=(ae_input), outputs=ae_output, name='autoencoder')
         self.autoencoder.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 
-        self.autoencoder.summary(expand_nested=True)
+        if verbose:
+            self.autoencoder.summary(expand_nested=True)
     
     # def build_RNN():
     #     # RNN = keras.RNN
@@ -108,6 +113,20 @@ class LED:
 
 
 
-autoencoder(tfkl.Dense(units=64,))
+NN = LED()
+
+input_shape = (10,10,2)
+latent_dim = 10
+
+conv = [(8,3),(16,3),(32,3)]
+dense = (64,128)
+
+
+NN.build_autoencoder(latent_dim, input_shape, dense, conv)
+
+
+
+
+
 
 
