d459e7d83389b2e6c7095860b6cc5f13f6f1613c
Catta76 <fede99catta@gmail.com>
2023-10-17 18:40:41 +0200

    Update mitchellschaeffer.py

--- scripts/datagen/mitchellschaeffer.py
+++ scripts/datagen/mitchellschaeffer.py
@@ -43,8 +43,31 @@ class MitchellSchaeffer(DataGen):
 
         t_begin = 0.
         delta_t = 20.
+        v = 0.05
         length_t = 2
 
+    
+        for x in range(self.grid_size[0]):
+            for y in range(self.grid_size[1]):
+                distance = np.sqrt((x-x0[0])**2+(y-x0[1])**2)
+                t_begin = distance/v
+                t_end = t_begin+length_t
+                
+                def f(u, t):
+                    I_app = self.I * \
+                        (t >= t_begin and t < t_end)
+                    res = [self.k*u[0]*(u[0] - self.alpha) * (1 - u[0]) - u[1] + I_app,
+                            self.epsilon*(u[0] - self.gamma*u[1])]
+                    return np.array(res)
+                
+                self.solver.f = f
+                self.solver.reset()
+                self.solver.generate()
+                if plot:
+                    self.solver.plot_solution()
+
+                self.sample[:, x, y] = self.solver.u.transpose()
+    
     
         current_explore = []
         explored = []
@@ -97,3 +120,35 @@ class MitchellSchaeffer(DataGen):
             x0 = (np.random.randint(0,self.grid_size[0]),np.random.randint(0,self.grid_size[1]))  
             self.generate_sample(x0,plot=plot)
         self.save_dataset(filename, format)
+
+
+
+
+
+# t_begin = 0.
+# delta_t = 20.
+# v = 0.05
+# length_t = 2
+
+# x0 = np.array([5,5])
+# grid_size = [10,10]
+
+# current_explore = []
+# explored = []
+# future_explore = [(x0[0],x0[1],t_begin)]
+
+# test = np.zeros(grid_size)
+
+
+# for x in range(grid_size[0]):
+#     for y in range(grid_size[1]):
+#         distance = np.sqrt((x-x0[0])**2+(y-x0[1])**2)
+#         t_begin = distance/v
+#         t_end = t_begin+length_t
+        
+#         test[x,y] = np.int32(t_begin)
+        
+
+# print(test)
+
+
