d664c62186cd795005c6c0b2e35cf82a7e21c537
ibismanu <ibis.manu@gmail.com>
2023-10-19 10:44:59 +0200

    parallel new real grid

--- scripts/datagen/mitchellschaeffer_parallel.py
+++ scripts/datagen/mitchellschaeffer_parallel.py
@@ -7,6 +7,7 @@ from datagen_parallel import DataGen
 from multiprocessing import Pool
 from particle.thetamethod import ThetaMethod
 import time
+from functools import partial
 
 class MitchellSchaeffer(DataGen):
 
@@ -45,50 +46,31 @@ class MitchellSchaeffer(DataGen):
         
         self.sample = np.zeros((self.num_it+1,grid_size[0],grid_size[1],len(params.u0)))
 
-    def generate_sample(self, sample_id, x0=None, plot=False):
+    def generate_sample(self, args):
 
-        #TODO metodo alternativo per calcolare I_app, griglia reale
-        # dato iniziale: velocità di propagazione della corrente
-        # t_begin = velocità*distanza
-        # t_end = velocità*distanza + length_t 
-        # loop sulla griglia per calcolare I_app
-        if x0 is None:
-            x0 = (np.random.randint(0,self.grid_size[0]),np.random.randint(0,self.grid_size[1]))
+        sample_id = args[0]
+        x0 = args[1]
+        plot = args[2]
+
+        if sample_id == 1:
+            print('Inside proc 1')
 
         t_begin = 0.
-        delta_t = 50.
+        v = 0.05
         length_t = 2
 
-    
-        current_explore = []
-        explored = []
-        future_explore = [(x0[0],x0[1],t_begin)]
+        if x0 is None:
+            x0 = (np.random.uniform(0,self.grid_size[0]),np.random.uniform(0,self.grid_size[1]))
 
-        while future_explore:  # same as while future_explore != []
-            current_explore = future_explore
-            future_explore = []
-
-            for point in current_explore:
-                above = (point[0]-1, point[1], point[2]+delta_t)
-                below = (point[0]+1, point[1], point[2]+delta_t)
-                left = (point[0], point[1]-1, point[2]+delta_t)
-                right = (point[0], point[1]+1, point[2]+delta_t)
-
-                if above[0] >= 0 and (above[0], above[1]) not in explored and above not in future_explore:
-                    future_explore.append(above)
-
-                if below[0] < self.grid_size[0] and (below[0], below[1]) not in explored and below not in future_explore:
-                    future_explore.append(below)
-
-                if left[1] >= 0 and (left[0], left[1]) not in explored and left not in future_explore:
-                    future_explore.append(left)
-
-                if right[1] < self.grid_size[1] and (right[0], right[1]) not in explored and right not in future_explore:
-                    future_explore.append(right)
-
+        for x in range(self.grid_size[0]):
+            for y in range(self.grid_size[1]):
+                distance = np.sqrt((x-x0[0])**2+(y-x0[1])**2)
+                t_begin = distance/v
+                t_end = t_begin+length_t
+                
                 def f(u, t):
                     I_app = self.I * \
-                        (t >= point[2] and t < point[2]+length_t)
+                        (t >= t_begin and t < t_end)
                     res = [self.k*u[0]*(u[0] - self.alpha) * (1 - u[0]) - u[1] + I_app,
                             self.epsilon*(u[0] - self.gamma*u[1])]
                     return np.array(res)
@@ -101,20 +83,20 @@ class MitchellSchaeffer(DataGen):
                 if plot:
                     solver.plot_solution()
 
-                self.sample[:, point[0], point[1]] = solver.u.transpose()
-
-                explored.append((point[0], point[1]))
+                self.sample[:, x, y] = solver.u.transpose()               
 
         filename = f'sample_{sample_id}.npy'
         np.save(filename, self.sample)
 
-    def generate_dataset_parallel(self, num_samples, num_processes):
+    def generate_dataset_parallel(self, num_samples, num_processes, x0=None, plot=False):
+
+        arg_list = [(sample_id,x0,plot) for sample_id in range(num_samples)]
 
         # Create a pool of processes
         start_time = time.perf_counter()
         with Pool(processes=num_processes) as pool:
             # Generate samples in parallel
-            pool.map(self.generate_sample, range(num_samples))
+            pool.map(self.generate_sample, arg_list)
         finish_time = time.perf_counter()
         print('Program finished in {} seconds'.format(finish_time-start_time))
 
